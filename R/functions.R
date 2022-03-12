66c6e1dfc9cb852e76a4f96d30328198539e74ea
Daniel Scarnecchia <scarnecchia@gmail.com>
2022-03-12 14:04:35 -0500

    Initial Code

---
+++ R/functions.R
@@ -0,0 +1,55 @@
+extract_total <- function(indsn, x) {
+  total <- indsn[[x]] %>% rvest::html_text2() %>%
+    stringr::str_extract("\\d+(?= \\b)") %>%
+    readr::parse_double()
+}
+
+extract_origin <- function(indsn, x) {
+  indsn[[x]] %>% rvest::html_element("img") %>%
+    rvest::html_attr("src") %>%
+    stringr::str_extract("(Flag_of_the_|Flag_of_)([a-zA-Z_]+|[a-zA-Z]+)") %>%
+    stringr::str_remove("(Flag_of_the_|Flag_of_)") %>%
+    stringr::str_replace_all("_", " ")
+}
+
+extract_counts <- function(indsn, x, condition) {
+  counts <- indsn[[x]] %>% rvest::html_text2() %>%
+    stringr::str_remove_all(".*(?=:)") %>%
+    stringr::str_remove_all(": ") %>%
+    stringr::str_remove_all("\\(") %>%
+    stringr::str_remove_all("\\)") %>%
+    stringr::str_remove_all("and")
+
+  if (condition == "captured") {
+    counts <- counts %>% stringr::str_extract_all("\\d+(?=, captured)")
+  } else if (condition == "damaged") {
+    counts <- counts %>% stringr::str_extract_all("\\d+(?=, damaged)")
+  } else if (condition == "destroyed") {
+    counts <- counts %>% stringr::str_extract_all("\\d+(?=, destroyed)")
+  } else {
+    print("Invalid Condition")
+  }
+
+  counts <- unlist(counts)
+  return(length(counts))
+}
+
+extract_system <- function(indsn, x) {
+  indsn[[x]] %>% rvest::html_text2() %>%
+    stringr::str_remove_all("^\\d+ ") %>%
+    stringr::str_extract(".*(?=:)")
+}
+
+extract_status <- function(indsn, x) {
+  indsn[[x]] %>%
+    rvest::html_text2() %>%
+    stringr::str_remove_all("\\(") %>%
+    stringr::str_remove_all("\\)") %>%
+    stringr::str_remove_all("\\d+") %>%
+    stringr::str_extract("destroyed|captured|abandoned|damaged|aboned")
+}
+
+extract_url <- function(indsn, x) {
+  indsn[[x]] %>%
+    rvest::html_attr("href")
+}
---
+++ scrape_oryx.R
@@ -0,0 +1,79 @@
+#' @title scrape_oryx
+#' @description A simple script for extracting tabular data from Oryx' excellent
+#'   post detailing materiel lost by all sides in the [Russian invasion of
+#'   Ukraine](https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html).
+#'
+#'
+#' @author Daniel Scarnecchia
+#'
+
+#' Setup
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(purrr)
+library(magrittr)
+library(tibble)
+library(stringr)
+library(readr)
+library(glue)
+source("R/functions.R")
+
+today <- format(Sys.Date(), "%Y-%m-%d")
+oryx <- rvest::read_html(
+  "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
+) %>% rvest::html_elements("article")
+
+materiel <- oryx %>% rvest::html_elements("li")
+
+#' Run Program
+data <-
+  tibble::tibble(
+    country = character(),
+    origin = character(),
+    system = character(),
+    status = character(),
+    url = character(),
+  )
+
+counter = 0
+for (a in seq_along(materiel)) {
+  status <- materiel[[a]] %>% rvest::html_elements("a")
+  for (b in seq_along(status)) {
+    counter = counter + 1
+    data[counter, 2] <- extract_origin(materiel, b)
+    data[counter, 3] <- extract_system(materiel, b)
+    data[counter, 4] <- extract_status(status, b)
+    data[counter, 5] <- extract_url(status, b)
+  }
+}
+
+readr::write_csv(data, file=glue::glue("outputfiles/data_{today}.csv"))
+
+data_wide <- data %>%
+  dplyr::select(country, origin, system, status) %>%
+  dplyr::group_by(origin, system, status) %>%
+  dplyr::summarise(count = n())
+
+readr::write_csv(data_wide, file=glue::glue("outputfiles/data_wide_{today}.csv"))
+
+# data <-
+#   tibble::tibble(
+#     country = character(),
+#     origin = character(),
+#     system = character(),
+#     captured = numeric(),
+#     damaged = numeric(),
+#     destroyed = numeric(),
+#     total = numeric()
+#   )
+#
+# for (i in seq_along(materiel)) {
+#   data[i, 2] <- extract_origin(materiel, i)
+#   data[i, 3] <- extract_system(materiel, i)
+#   data[i, 4] <- extract_counts(materiel, i, "captured")
+#   data[i, 5] <- extract_counts(materiel, i, "damaged")
+#   data[i, 6] <- extract_counts(materiel, i, "destroyed")
+#   data[i, 7] <- extract_total(materiel, i)
+# }
