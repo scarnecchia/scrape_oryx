63fbb7c2968cd1df3a51a1387765726acce6741d
Daniel Scarnecchia <scarnecchia@gmail.com>
2022-03-19 15:55:52 -0400

    DEV-6: Modularized code and added @ajnafa's totals_by_category code

--- scrape_oryx.R
+++ scrape_oryx.R
@@ -21,54 +21,26 @@ library(readr)
 library(glue)
 library(logr)
 source("R/functions.R")
+source("R/scrape_data.R")
+source("R/totals_by_type.R")
 
-
-tmp <- file.path("outputfiles", sprintf("scrape_oryx_%s.log", format(Sys.time(), "%Y%m%dT%H%M%S")))
+tmp <-
+  file.path("outputfiles", sprintf("scrape_oryx_%s.log", format(Sys.time(), "%Y%m%dT%H%M%S")))
 lf <- logr::log_open(tmp)
 today <- format(Sys.Date(), "%Y-%m-%d")
 
-oryx <- rvest::read_html(
-  "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
-) %>% rvest::html_elements("article")
+totals_by_system <- scrape_data() %>%
+  readr::write_csv(., file = glue::glue("outputfiles/totals_by_system_{today}.csv"))
 
-materiel <- oryx %>% rvest::html_elements("li")
-
-# Retreive the start position of each country
-country_pos <- materiel %>% rvest::html_text2() %>%
-  # T-64BV is the first row in the tank list and marks the beginning of each country
-  stringr::str_which("T-64BV")
-
-#' Run Program
-data <-
-  tibble::tibble(
-    country = character(),
-    origin = character(),
-    system = character(),
-    status = character(),
-    url = character(),
-  )
-
-counter = 0
-for (a in seq_along(materiel)) {
-  status <- materiel[[a]] %>% rvest::html_elements("a")
-  for (b in seq_along(status)) {
-    counter = counter + 1
-    data[counter, 1] <- ifelse(a < country_pos[2], "Russia", "Ukraine")
-    data[counter, 2] <- extract_origin(materiel, a)
-    data[counter, 3] <- extract_system(materiel, a)
-    data[counter, 4] <- extract_status(status, b)
-    data[counter, 5] <- extract_url(status, b)
-  }
-}
-
-readr::write_csv(data, file = glue::glue("outputfiles/data_{today}.csv"))
-
-data_wide <- data %>%
+totals_by_system_wide <- totals_by_system %>%
   dplyr::select(country, system, status) %>%
   dplyr::group_by(country, system, status) %>%
-  dplyr::summarise(count = n())
+  dplyr::summarise(count = n()) %>%
+  readr::write_csv(.,
+                   file = glue::glue("outputfiles/totals_by_system_wide_{today}.csv"))
 
-readr::write_csv(data_wide, file = glue::glue("outputfiles/data_wide_{today}.csv"))
+total_by_type <- totals_by_type() %>%
+  readr::write_csv(., file = glue::glue("outputfiles/totals_by_type_{today}.csv"))
 
 logr::log_code()
 logr::log_close()
